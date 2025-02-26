3323a573c2ddd609687ad6adc62a732575443a7b
Emmanuel Antônio <emmanuel_braga@outlook.com>
2025-02-25 22:45:38 -0300

    Segundo commit do projeto na branch default

---
+++ GestaoEquipamentos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GestaoEquipamentos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
